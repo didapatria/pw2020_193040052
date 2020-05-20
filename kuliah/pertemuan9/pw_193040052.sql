6e1fe69eee00654082dec7d5b0911407e5a6d85b
didapatria <didapatria3@gmail.com>
2020-05-21 02:45:23 +0700

    Edit sql

--- kuliah/pertemuan9/pw_193040052.sql
+++ kuliah/pertemuan9/pw_193040052.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 20, 2020 at 02:12 PM
+-- Generation Time: May 20, 2020 at 08:37 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.5
 
